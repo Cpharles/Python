ccf5f5f92dfa0292e7417269f6bc4cadf4cfad8f
Charles <bandcoult@gmail.com>
2020-02-29 18:24:57 -0300

    corrigido

--- Curso em Video/ex006.py
+++ Curso em Video/ex006.py
@@ -3,7 +3,7 @@
 n = int(input('Entre com um número:_ '))
 db = n * 2
 tr = n * 3
-raiz = n ** 1/2
+raiz = n ** (1/2)
 print('O dobro do numero {} é:_ {}'.format(n, db))
 print('O tripo do numero {} é:_ {}'.format(n, tr))
 print('A raiz quadada do número {} é:_ {}'. format(n, raiz))
--- Curso em Video/ex011.py
+++ Curso em Video/ex011.py
@@ -6,5 +6,6 @@ print('obs.:fornecer o valor em metros')
 H = float(input('Forneça a altura da parede: '))
 L = float(input('Forneça a largura da parede: '))
 s = H * L
+print('Sua parede tem uma dimensão de {}x{} e sua área é de {}m2'.format(H, L, s))
 Qntinta = s / 2
-print('Será necessário {:0.1f}lts para pintar {}mts2'.format(Qntinta, s))
+print('Será necessário {:0.1f}lts para pintar {}m2'.format(Qntinta, s))
